ab0b94db682c1f941e89f3466729da14b8901e64
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-08-25 22:57:09 +0530

    doctor entity

--- src/main/java/com/sasmitha/nexta/entity/Doctor.java
+++ src/main/java/com/sasmitha/nexta/entity/Doctor.java
@@ -1,4 +1,24 @@
 package com.sasmitha.nexta.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "doctors")
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String specialization;
+
+    @OneToOne
+    private User user;
 }
