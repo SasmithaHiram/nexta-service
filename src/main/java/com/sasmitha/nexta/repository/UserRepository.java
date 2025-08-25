260d54343d36a6fa9b08fffc10a3a6ad035af874
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-08-25 23:34:01 +0530

    repositories

---
+++ src/main/java/com/sasmitha/nexta/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sasmitha.nexta.repository;
+
+import com.sasmitha.nexta.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
