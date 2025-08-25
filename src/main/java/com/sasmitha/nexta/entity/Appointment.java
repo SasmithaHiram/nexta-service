72097e78cdee59eb10ed3b69b02aee8f89506b33
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-08-25 23:22:32 +0530

    appointment entity

--- src/main/java/com/sasmitha/nexta/entity/Appointment.java
+++ src/main/java/com/sasmitha/nexta/entity/Appointment.java
@@ -1,4 +1,37 @@
 package com.sasmitha.nexta.entity;
 
+import com.sasmitha.nexta.util.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "appointments")
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @ManyToOne
+    private Patient patient;
+
+    private LocalDate date;
+
+    private LocalTime time;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
 }
---
+++ src/main/java/com/sasmitha/nexta/util/Status.java
@@ -0,0 +1,7 @@
+package com.sasmitha.nexta.util;
+
+public enum Status {
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
