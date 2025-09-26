128ddd5bf97a0e9685e78553bc98b66d9f1956c1
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-09-26 23:34:31 +0530

    f

--- src/main/java/com/sasmitha/lms/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sasmitha.lms.repository;
-
-import com.sasmitha.lms.entity.ClassroomEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<ClassroomEntity, Long> {
-}
