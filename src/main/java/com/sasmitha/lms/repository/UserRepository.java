5d46f5c97265dfbf3ad1d52f28fd1f0edfbd81ba
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-10-07 21:33:16 +0530

    shopping cart backend

--- src/main/java/com/sasmitha/lms/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sasmitha.lms.repository;
-
-import com.sasmitha.lms.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
